3db37d31b6dee27aedacf010e5d7e5c4345f56ed
Publikwerker <hello@publikwerks.com>
2020-02-18 14:28:37 -0800

    created a mock database to test mongo

--- tstc-trial/app.js
+++ tstc-trial/app.js
@@ -1,19 +1,13 @@
 // Full Documentation - https://docs.turbo360.co
 const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
 
-
-/*  
-	Apps can also be initialized with config options as shown in the commented out example below. Options
-	include setting views directory, static assets directory, and database settings. To see default config
-	settings, view here: https://docs.turbo360.co
-
-*/
 const config = {
 	views: 'views', 		// Set views directory 
 	static: 'public', 		// Set static assets directory
 	db: { 					// Database configuration. Remember to set env variables in .env file: MONGODB_URI, PROD_MONGODB_URI
-		url: (process.env.TURBO_ENV == 'dev') ? process.env.MONGODB_URI : process.env.PROD_MONGODB_URI,
-		type: 'mongo',
+		//url: (process.env.TURBO_ENV == 'dev') ? process.env.MONGODB_URI : process.env.PROD_MONGODB_URI,
+    url: 'mongodb://localhost/tstc-trial',
+    type: 'mongo',
 		onError: (err) => {
 			console.log('DB Connection Failed!')
 		},
---
+++ tstc-trial/models/Profile.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const Profile = new mongoose.Schema({
+  firstname: {
+    type:String,
+    trim:true,
+    default:''
+  },
+  lastname: {
+    type:String,
+    trim: true,
+    default:''
+  },
+  age: {
+    type:Number,
+    default:0
+  },
+  team: {
+    type:String,
+    trim: true,
+    default:''
+  },
+  position: {
+    type:String,
+    trim: true,
+    default:''
+  },
+});
+
+module.exports = mongoose.model('Profile', Profile);
