4c94f4efae0be793355eec65c0d8cb52dd8e8522
Publikwerker <hello@publikwerks.com>
2020-02-24 13:28:30 -0800

    client is operational!

--- tstc-trial/client/src/Compendium.js
+++ tstc-trial/client/src/Compendium.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import Ticker from '.components/Ticker.js';
+import Ticker from './components/Ticker.js';
 
 export default class Compendium extends Component {
   
   render() {
     return (
       <div className="compendium">
-        <Switch>
           <p>TSTC is Online!</p>
           <Ticker />
-        </Switch>
       </div>
     );
   }
