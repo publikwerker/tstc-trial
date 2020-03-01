4d74f3003fa8e53d90d26ae845ff83e519d9ed80
Publikwerker <hello@publikwerks.com>
2020-02-29 17:13:16 -0800

    styled ticker box to look like old-school netscape marquee

--- tstc-trial/client/src/components/Ticker.js
+++ tstc-trial/client/src/components/Ticker.js
@@ -47,14 +47,18 @@ export default class Ticker extends Component {
 
     return (
       <div className="banner-box">
-      <p>Notice: Your browser is exposing the following information:</p>
-      <div id="ticker"       
-        style={{
-          width: '100%',
-          whiteSpace: 'nowrap',
-        }}>
-        <Marquee>navigator.appName is  "{navAppName}". navigator.appCodeName is "{navAppCodeName}". navigator.platform is "{navPlatform}". navigator.product is "{navProduct}". navigator.appVersion is "{navAppVersion}". navigator.userAgent is "{navUserAgent}". navigator.language is "{navLanguage}". navigator.onLine is "{navOnLine}". navigator.javaEnabled() is "{navJavaEnabled}". Browser inner window width: {w}, height: {h}. Your page hostname is "{hostname}". Your location is "{visitorLocation}".</Marquee>
-      </div>
+        <div className="ticker-box">
+          <div id="ticker"       
+            style={{
+              width: '100%',
+              padding: '5px',
+              fontSize: '1.3rem',
+              whiteSpace: 'nowrap',
+              direction: 'left',
+            }}>
+            <Marquee>Notice: Your browser is exposing the following information: The navigator.appName is  "{navAppName}". The navigator.appCodeName is "{navAppCodeName}". The navigator.platform is "{navPlatform}". The navigator.product is "{navProduct}". The navigator.appVersion is "{navAppVersion}". The navigator.userAgent is "{navUserAgent}". The navigator.language is "{navLanguage}". The navigator.onLine is "{navOnLine}". The navigator.The javaEnabled() is "{navJavaEnabled}". The browser inner window width: {w}, height: {h}. Your page hostname is "{hostname}". Your location is "{visitorLocation}".</Marquee>
+          </div>
+        </div>
       </div>
     );
   }
