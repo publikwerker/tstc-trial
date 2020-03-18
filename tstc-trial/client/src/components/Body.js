d6a92e5227894c201e4e593540ad748613097e1a
Publikwerker <hello@publikwerks.com>
2020-03-17 20:57:19 -0700

    Created Body and Footer Components. Inside Body are divs for blogs and user stories, which will both eventually break out into components of their own.

---
+++ tstc-trial/client/src/components/Body.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Body () {
+  return (
+    <main>
+      <div className="blogs">
+        <h2>This will be the blogs.</h2>
+      </div>
+      <div className="stories">
+        <h2>This will be user stories.</h2>
+      </div>
+    </main>
+  )
+}
--- tstc-trial/client/src/components/Compendium.js
+++ tstc-trial/client/src/components/Compendium.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Header from './Header';
+import Body from './Body';
+import Footer from './Footer';
 
 export default class Compendium extends Component {
   
   render() {
     return (
       <div className="compendium">
-          <p>TSTC is Online!</p>
           <Header />
+          <Body />
+          <Footer />
       </div>
     );
   }
