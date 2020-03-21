c9c2a2c50d7848784ed9ee94ff55b20ca3817508
Publikwerker <hello@publikwerks.com>
2020-03-20 20:21:45 -0700

    Reconfigured Blog component to conditionally reveal blog text. OnClick does not work, yet.

--- tstc-trial/client/src/components/Blog.js
+++ tstc-trial/client/src/components/Blog.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 
 export default class Blog extends Component {
   constructor(props){
-    super(props);
+    super(props)
     this.state = {
       expand: false,
       ...props.data
@@ -11,26 +11,22 @@ export default class Blog extends Component {
 
   handleClick = (e) => {
     e.preventDefault();
-    this.setState({expand: !this.state.expand});
-  }
-
-  buttonMade = () => {
-    if (this.state.expand===true) {
-      return (<div className="hidden"></div>)
-    } else {
-      return (<button onClick={e=>this.handleClick(e)}>Less</button>);
-    }
+    this.setState((state) => ({expand: !this.state.expand}));
   }
 
   render () {
     console.log('blog ran');
     console.log(this.state);
-    return (
-      <div className="oneBlog">
-        <h2 className="oneBlog_title" onClick={e=>this.handleClick(e)}>{this.state.title}</h2>
-        <p className="oneBlog_body">{this.state.content}</p>
-        {this.buttonMade()}
-      </div>
-    )
+      if (this.state.expand===false) {
+        return (<div className="oneBlog" onClick={e=>this.handleClick(e)}>
+        <h2 className="oneBlog_title">{this.state.title}</h2>
+      </div>)
+      } else {
+        return (<div className="oneBlog" onClick={e=>this.handleClick(e)}>
+          <h2 className="oneBlog_title">{this.state.title}</h2>
+          <p className="oneBlog_body">{this.state.content}</p>
+          <button onClick={e=>this.handleClick(e)}>Less</button>
+        </div>)
+      }
   }
 }
