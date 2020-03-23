e564157e47fd2141a582ba5a2b3684b3bdba3a0e
Publikwerker <hello@publikwerks.com>
2020-03-22 18:51:44 -0700

    Created components for Collective and Media.

--- tstc-trial/client/src/components/Body.js
+++ tstc-trial/client/src/components/Body.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Blogs from './Blogs';
 import Stories from './Stories';
+import Media from './Media';
+import Collective from './Collective';
 
 export default function Body () {
   return (
     <main>
       <Blogs />
       <Stories />
+      <Collective />
+      <Media />
     </main>
   )
 }
---
+++ tstc-trial/client/src/components/Collective.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Collective () {
+  return (
+    <div>
+      <p>This is for the collective experience.</p>
+    </div>
+  )
+}
