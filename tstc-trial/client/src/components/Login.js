19efe5d8db52e12967a474cea9363b07692ecd11
Jason Hoffman <hello@publikwerks.com>
2021-01-07 22:25:24 -0800

    created a loginForm element that is rendered when login button is clicked

--- tstc-trial/client/src/components/Login.js
+++ tstc-trial/client/src/components/Login.js
@@ -4,14 +4,25 @@ import React from 'react';
 export default function Login(props) {
   console.log(props, "in Login");
 
+  const loginForm = <div></div>;
   const sayTheWord = (e) => {
     e.preventDefault();
     console.log("click");
+    loginForm = <form className="login-form">
+      <label for="username">Username</label>
+      <input type="text" name="username" placeholder="username"></input>
+
+      <label for="password">Password</label>
+      <input type="text" name="password" placeholder="password"></input>
+      
+      <button type="submit">Login</button>
+    </form>
   }
   return(
       <div className="row">
         <button className="button" onClick={props.handleClick}>About</button>
         <button className="button" onClick={sayTheWord}>Login</button>
+        {loginForm}
       </div>
   )
 };
