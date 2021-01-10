7595ea75ba32f9a71122f04ed2945ee176c2fc16
Jason Hoffman <hello@publikwerks.com>
2021-01-09 18:45:23 -0800

    created db folder, moved schema and seeds into it. created config folder with orm file and moved mysql connection code into connection.js

---
+++ tstc-trial/config/connection.js
@@ -0,0 +1,30 @@
+//Set up MySQL Connection
+const mysql = require("mysql");
+
+//Import variables
+const HOST = process.env.MYSQL_CONNECTION_HOST;
+const PORT = process.env.MYSQL_CONNECTION_PORT;
+const USER = process.env.MYSQL_CONNECTION_USER;
+const PASSWORD = process.env.MYSQL_CONNECTION_PASSWORD;
+const MYSQLDB = process.env.MYSQL_DB;
+
+//MySQL DB Connection Information 
+var MYSQLConnection = mysql.createConnection({
+  host: HOST,
+  port: PORT,
+  user: USER,
+  password: PASSWORD,
+  database: "visitor_log_db"
+});
+
+// Initiate MySQL Connection.
+MYSQLConnection.connect(function(err) {
+  if (err) {
+    console.error("error connecting to MYSQL: " + err.stack);
+    return;
+  }
+  console.log("connected to MYSQL as id " + MYSQLConnection.threadId);
+  });
+
+// Export connection for ORM to use.
+module.exports = MYSQLconnection;
